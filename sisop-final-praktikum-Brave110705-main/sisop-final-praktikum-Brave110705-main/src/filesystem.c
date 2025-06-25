b97ef8045b2e4875689d8042774636caf81a5374
Brave <132269905+Brave110705@users.noreply.github.com>
2025-06-25 17:25:57 +0700

    task 2 (gtw debugnya gmn, semoga bisa)

--- sisop-final-praktikum-Brave110705-main/sisop-final-praktikum-Brave110705-main/src/filesystem.c
+++ sisop-final-praktikum-Brave110705-main/sisop-final-praktikum-Brave110705-main/src/filesystem.c
@@ -17,20 +17,54 @@ void fsRead(struct file_metadata* metadata, enum fs_return* status) {
   struct node_fs node_fs_buf;
   struct data_fs data_fs_buf;
 
-  /**
-   * add local variable here
-   * ...
-   */
+  int i, j; //buat loop
+  int ada = 0; //sesuai nama, kalok ada = 1
+  int node_index = -1; //buat nyari index, mulai dari -1 sesuai ketentuanny
 
-  readSector(&data_fs_buf, FS_DATA_SECTOR_NUMBER);
-  readSector(&(node_fs_buf.nodes[0]), FS_NODE_SECTOR_NUMBER);
-  readSector(&(node_fs_buf.nodes[32]), FS_NODE_SECTOR_NUMBER);
+  readSector(&(node_fs_buf.nodes[0]), FS_NODE_SECTOR_NUMBER);        
+  readSector(&(node_fs_buf.nodes[32]), FS_NODE_SECTOR_NUMBER);   
+  readSector(&data_fs_buf, FS_DATA_SECTOR_NUMBER);                   
 
-  /**
-   *  add your code here
-   * ...
-   */
+  //sesuai soal, cari secara iteratif "mencari node yang memiliki nama yang sesuai dengan metadata->node_name dan parent index sesuai dengan metadata->parent_index"
+  for (i = 0; i < FS_MAX_NODE; i++) {
+    struct node_item* node = &node_fs_buf.nodes[i];
+    if (node->parent_index == metadata->parent_index &&
+        strncmp(node->node_name, metadata->node_name, MAX_FILENAME) == 0) {
+      ada = 1;
+      node_index = i;
+      break;
+    }
+  }
+
+  //sesuai soal, kalok ga nemu = set status dengan FS_R_NODE_NOT_FOUND
+  if (!ada) {
+    *status = FS_R_NODE_NOT_FOUND;
+    return;
+  }
+
+  //sesuai soal, cek dulu direktori/file
+  if (node_fs_buf.nodes[node_index].data_index == FS_NODE_D_DIR) {
+    *status = FS_R_TYPE_IS_DIRECTORY;
+    return;
+  }
+
+  // ngikut soal
+  metadata->filesize = 0;
+  byte data_index = node_fs_buf.nodes[node_index].data_index;
+
+  //ngikut soal, secara iteratif pake readSector buat baca data dari sektor yang ditunjuk oleh data pada data index dengan sectors ke-i 
+  for (i = 0; i < FS_MAX_SECTOR; i++) {
+    byte sector_number = data_fs_buf.datas[data_index].sectors[i];
+    if (sector_number == 0x00) break; //di-break sesuai soal
+
+  //sesuai  soal, disimpan ke dalam metadata->buffer + i * SECTOR_SIZE
+    readSector(metadata->buffer + i * SECTOR_SIZE, sector_number);
+    metadata->filesize += SECTOR_SIZE;
+  }
+
+  *status = FS_SUCCESS;
 }
+
 
 // TODO: 3. Implement fsWrite function
 void fsWrite(struct file_metadata* metadata, enum fs_return* status) {
